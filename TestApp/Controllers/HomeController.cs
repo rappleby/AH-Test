5cd248172d2e055278af5008d32af69455bd444c
Richard Appleby <richard.appleby@nccgroup.com>
2014-10-20 15:32:27 +0100

    blah

--- TestApp/Controllers/HomeController.cs
+++ TestApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TestApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to gitub testing!";
 
             return View();
         }
